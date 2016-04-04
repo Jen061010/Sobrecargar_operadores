d95bbd5d438782aba5bd897763cf9ebb4668d565
Jennifer <jvs1192@gmail.com>
2016-04-04 12:15:52 +0200

    First Commit

---
+++ Sobrecargar_operadores/Alimentos.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace Sobrecargar_operadores
+{
+    public class Alimentos
+    {
+        
+        public int Temperatura { get; protected set; }
+        public static Alimentos operator +(Alimentos a1, Alimentos a2) {
+            
+            return new Alimentos(){Temperatura=a1.Temperatura+a2.Temperatura};
+        }
+    }
+}
---
+++ Sobrecargar_operadores/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sobrecargar_operadores
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+
+            var totalTemperatura = new Pan() + new Lasania();
+            Console.WriteLine(totalTemperatura.Temperatura);
+            Console.ReadLine();
+        }
+    }
+}
